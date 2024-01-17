85f7883c44b4c9c3c039384fe241e24b41b05a88
J Caso <jcaso@protonmail.com>
2024-01-17 14:25:33 +0100

    fix: routes types

--- src/ui/components/Router/routes.tsx
+++ src/ui/components/Router/routes.tsx
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from 'react';
+import React, {lazy, ReactNode, Suspense} from 'react';
 import { Navigate, RouteObject, useRoutes } from 'react-router-dom';
 import { useAuth } from '@components/Router/AuthProvider';
 
@@ -9,13 +9,18 @@ const SessionDetails = lazy(
 const Connect = lazy(() => import('@pages/popup/connect/Connect'));
 const Lock = lazy(() => import('@pages/popup/lock/Lock'));
 
-const ProtectedRoute = ({ children }) => {
+type ProtectedRouteProps = {
+  children: ReactNode;
+};
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
   const { isLoggedIn } = useAuth();
+
   if (!isLoggedIn) {
-    return <Navigate to="/lock" />;
+    return <Navigate to="/lock" replace />;
   }
 
-  return children;
+  return <>{children}</> || null;
 };
 
 export const routes: Array<RouteObject> = [
