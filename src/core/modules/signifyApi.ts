ac75efc44c5fe41d29d97977a462375897af2214
J Caso <jcaso@protonmail.com>
2024-01-17 14:19:19 +0100

    feat: add .env file example

--- src/core/modules/signifyApi.ts
+++ src/core/modules/signifyApi.ts
@@ -3,8 +3,8 @@ import { SignifyClient, ready, Tier, randomPasscode } from 'signify-ts';
 class SignifyApi {
   private signifyClient!: SignifyClient;
   public started: boolean;
-  static readonly KERIA_URL: string = 'http://192.168.0.66:3901';
-  static readonly KERIA_BOOT_ENDPOINT = 'http://192.168.0.66:3903';
+  static readonly KERIA_URL = import.meta.env.VITE_KERIA_URL;
+  static readonly KERIA_BOOT_ENDPOINT = import.meta.env.VITE_KERIA_BOOT_ENDPOINT;
   static readonly SIGNIFY_BRAN_STORAGE_KEY = 'SIGNIFY_BRAN';
 
   constructor() {
@@ -14,29 +14,20 @@ class SignifyApi {
     await ready();
     const bran = await this.getBran();
 
-    console.log('bran from storage');
-    console.log(bran);
-
     this.signifyClient = new SignifyClient(
-      SignifyApi.KERIA_URL,
+      SignifyApi.KERIA_URL as string,
       bran,
       Tier.low,
       SignifyApi.KERIA_BOOT_ENDPOINT,
     );
 
-    console.log('signifyClient initialized');
-    console.log(this.signifyClient);
     try {
       await this.signifyClient.connect();
       this.started = true;
     } catch (err) {
-      console.log('err');
-      console.log(err);
       await this.signifyClient.boot();
-      console.log('after boot, lets try to connect');
       await this.signifyClient.connect();
       this.started = true;
-      console.log('hey connected!');
     }
   }
   private async getBran(): Promise<string> {
