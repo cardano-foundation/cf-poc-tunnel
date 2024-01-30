ecd0e97901c13ba24a40faa50960dfffbeefab56
Sotatek-HocNguyena <hoc.nguyena@sotatek.com>
2024-01-30 16:43:54 +0700

    feat: change signify-resource value to server AID

--- demo/server/src/middlewares/signResponse.middleware.ts
+++ demo/server/src/middlewares/signResponse.middleware.ts
@@ -1,18 +1,16 @@
 import { Request, Response } from "express";
-import { getServerAuthn, getIdentifierByName, getServerSignifyController } from "../modules/signifyApi";
+import { getServerAuthn, getIdentifierByName } from "../modules/signifyApi";
 import { config } from "../config";
 
 export const signResponse = async (req: Request, res: Response, next) => {
     const serverAID = await getIdentifierByName(config.signifyName);
     const authn = await getServerAuthn();
-    const signifyController = await getServerSignifyController();
     // Intercept the response
     const originalSend = res.send;
     res.send = function (body) {
         const originalHeaders = res.getHeaders();
         const headers = new Headers(originalHeaders);
-        headers.set("aid", JSON.stringify({ aid: serverAID }));
-        headers.set('Signify-Resource', signifyController.pre);
+        headers.set('Signify-Resource', serverAID.prefix);
         headers.set(
             'Signify-Timestamp',
             new Date().toISOString().replace('Z', '000+00:00')
@@ -25,7 +23,6 @@ export const signResponse = async (req: Request, res: Response, next) => {
         }
         const signedHeaders = authn?.sign(headers, req.method, req.path.split('?')[0]);
         signedHeaders?.forEach((value, key) => {
-            console.log({ key, value })
             res.set(key, value);
         });
         originalSend.call(this, JSON.stringify(body));
