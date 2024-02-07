3a4c65e1b49eaeabe9fb448d693e7a6a2adf9edd
J Caso <jcaso@protonmail.com>
2024-02-07 17:40:48 +0100

    fix: sign and verify with serverAID

--- demo/server/src/middlewares/verifyRequest.middleware.ts
+++ demo/server/src/middlewares/verifyRequest.middleware.ts
@@ -1,18 +1,28 @@
-import { Request, Response } from "express";
-import { getServerAuthn } from "../modules/signifyApi";
+import { Request, Response } from 'express';
+import {getIdentifierByName, getSigner} from '../modules/signifyApi';
+import {config} from "../config";
+import {Authenticater} from "signify-ts";
 
 export const verifyRequest = async (req: Request, res: Response, next) => {
-    const authn = await getServerAuthn();
+    const serverAID = await getIdentifierByName(config.signifyName);
+
+    const signer = await getSigner(serverAID);
+
+    const authenticator = new Authenticater(
+        signer.signers[0],
+        signer.signers[0].verfer,
+    );
+
     try {
-        console.log('verifying...')
-        const verification = authn?.verify(
+        console.log('verifying...');
+        const verification = authenticator?.verify(
             new Headers(req.headers),
             req.method,
             req.path.split('?')[0],
-        )
+        );
         console.log({ verification });
         if (!verification) {
-            res.status(400).send("Request was not signed correctly");
+            res.status(400).send('Request was not signed correctly');
         } else {
             next();
         }
@@ -20,4 +30,4 @@ export const verifyRequest = async (req: Request, res: Response, next) => {
         console.log({ error });
         res.status(500).send((error as Error).message);
     }
-  }
+};
