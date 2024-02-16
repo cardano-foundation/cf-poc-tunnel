38b076832f51999519eb16bab827d4ca16398602
Sotatek-HocNguyena <hoc.nguyena@sotatek.com>
2024-02-16 16:47:20 +0700

    feat: use sqlite to save session

--- demo/server/src/database/index.ts
+++ demo/server/src/database/index.ts
@@ -3,12 +3,8 @@ dotenv.config();
 import { DataSource } from "typeorm";
 
 export const dataSource = new DataSource({
-    type: "postgres",
-    host: process.env.DB_HOST ? process.env.DB_HOST : 'localhost',
-    port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
-    username: process.env.DB_USERNAME ? process.env.DB_USERNAME : 'postgres',
-    password: process.env.DB_PASSWORD ? process.env.DB_PASSWORD : 'postgres',
-    database: process.env.DB_DATABASE ? process.env.DB_DATABASE : 'cf-poc-tunnel',
+    type: "sqlite",
+    database: process.env.DB_DATABASE ? process.env.DB_DATABASE : 'cf-poc-tunnel.sql',
     entities: ["src/database/entities/*.ts"],
     synchronize: process.env.NODE_ENV === 'production' ? false : true
   });
