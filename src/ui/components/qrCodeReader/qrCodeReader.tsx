71238d0b919d107dbd530dc35aacafaf8fdf01ca
J Caso <jcaso@protonmail.com>
2024-02-26 15:46:54 +0100

    feat: add QrCodeReader

---
+++ src/ui/components/qrCodeReader/qrCodeReader.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import {QrScanner} from '@yudiel/react-qr-scanner';
+import "./qrCodeReader.scss";
+
+const QrCodeReader = () => {
+    const [data, setData] = useState<string>('No QR code detected');
+    const [error, setError] = useState<string | null>(null);
+    const [isCameraActive, setIsCameraActive] = useState<boolean>(false);
+
+    const handleScan = (data: string | null) => {
+        if (data) {
+            setData(data);
+            console.log(`QR Code detected: ${data}`);
+        }
+    };
+
+    const handleError = (err: any) => {
+        console.error(err);
+        setError('Failed to access the camera. Please check device permissions.');
+        setIsCameraActive(false);
+    };
+
+    return (
+        <div>
+            {isCameraActive ? (
+                <QrScanner
+                    onError={handleError}
+                    onDecode={handleScan}
+                />
+            ) : (
+                <p>{error || 'Camera access is not active.'}</p>
+            )}
+            <p>{data}</p>
+        </div>
+    );
+};
+
+export { QrCodeReader };
