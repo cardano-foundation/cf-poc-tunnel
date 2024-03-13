aa9c3dab4467fae5ea3901795d4d7ff1ceb2da08
J Caso <jcaso@protonmail.com>
2024-03-13 17:25:14 +0100

    fix: header styles

--- demo/ui/src/components/Header.tsx
+++ demo/ui/src/components/Header.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 const Header: React.FC = () => {
     return (
-        <header className="bg-gray-900 text-white body-font shadow w-full">
+        <header className="text-white body-font shadow w-full fixed top-0 left-0 z-50 bg-black bg-opacity-60">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                 <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
                     <span className="ml-3 text-xl">Demo IIW</span>
                 </a>
                 <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-                    <a href="#" className="mr-5 hover:text-gray-400">Main</a>
-                    <a href="#" className="mr-5 hover:text-gray-400">About</a>
-                    <a href="#" className="mr-5 hover:text-gray-400">Contact</a>
+                    <a href="#" className="mr-5 text-white hover:text-gray-300">About</a>
+                    <a href="#" className="mr-5 text-white hover:text-gray-300">Demo</a>
+                    <a href="#" className="mr-5 text-white hover:text-gray-300">Contact</a>
                 </nav>
             </div>
         </header>
--- demo/ui/src/components/HeroSection.tsx
+++ demo/ui/src/components/HeroSection.tsx
@@ -3,7 +3,7 @@ const HeroSection: React.FC = () => {
     return (
         <>
             <div className="relative h-screen flex items-center justify-center text-center bg-hero bg-cover bg-no-repeat" >
-                <div className="hero-content z-10 p-24 bg-black bg-opacity-70 rounded-lg">
+                <div className="hero-content z-10 p-24 bg-black bg-opacity-80 rounded-lg">
                     <p className="text-sm md:text-base text-white font-bold mb-2">WELCOME <span className="text-gray-200">/ Demo Day</span></p>
                     <h1 className="font-bold break-normal text-white text-3xl md:text-5xl mb-4">Explore Digital Identity with Us</h1>
                     <p className="text-base md:text-xl text-gray-300 font-bold mb-8">Discover innovative solutions by Cardano Foundation</p>
