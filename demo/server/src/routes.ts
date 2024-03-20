8f5c601a80a3ba0ea5094a049c05bb0e6d8c5a81
iFergal <fergaloconnor1998@gmail.com>
2024-03-20 11:48:49 +0000

    feat(server): logout endpoint

--- demo/server/src/routes.ts
+++ demo/server/src/routes.ts
@@ -4,11 +4,13 @@ import { ping } from "./apis/ping.api";
 import { getServerOOBI, resolveClientOOBI } from "./apis/oobi.api";
 import { getSchema } from "./apis/schema.api";
 import { discloseAcdc } from "./apis/discloseAcdc.api";
+import { logout } from "./apis/logout.api";
 import { decryptVerifyRequest, encryptSignResponse, verifySession } from "./middlewares";
 import { getAcdcRequirements } from "./apis/acdcRequirements.api";
 
 export const router = express.Router();
 router.post(config.path.ping, verifySession(["user"]), decryptVerifyRequest, ping, encryptSignResponse); // POST to test ESSR
+router.post(config.path.ping, verifySession(["user"]), decryptVerifyRequest, logout, encryptSignResponse);
 router.post(config.path.resolveOOBI, resolveClientOOBI);
 router.get(config.path.oobi, getServerOOBI);
 router.get(config.path.schema, getSchema);
