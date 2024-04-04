1bbcb6a44825a7903591559efd593995a19e0cbf
iFergal <fergaloconnor1998@gmail.com>
2024-04-04 17:33:20 +0100

    build: serve UI with nginx

--- demo/ui/vite.config.ts
+++ demo/ui/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,6 +8,7 @@ export default defineConfig({
     react({
       include: "**/*.tsx",
     }),
+    nodePolyfills(),
   ],
   server: {
     host: true,
